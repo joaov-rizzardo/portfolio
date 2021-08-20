df24219c84c6e227d323b2b2abcacc875b086709
João Victor Domingues Rizzardo <81448179+joaov-rizzardo@users.noreply.github.com>
2021-08-20 01:19:38 -0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,16 @@
+$('nav a, #vejaMais').click(function(e){
+    e.preventDefault();
+
+    let id = $(this).attr('href')
+
+    let altura = $(id).offset().top;
+
+    let menuHeight = $('nav').innerHeight();
+
+    $('html').animate({
+        scrollTop: altura - menuHeight
+    },20);
+
+})
+
+
