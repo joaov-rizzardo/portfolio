524fb851a5ed971538f4a9dab006712b340db65d
João Victor Domingues Rizzardo <81448179+joaov-rizzardo@users.noreply.github.com>
2021-09-26 12:36:51 -0300

    Add files via upload

---
+++ js/projetos.js
@@ -0,0 +1,5 @@
+$(document).ready(()=> {
+    let menuHeight = $('#navegacao-biblioteca').innerHeight()
+
+    $('#fotos').css('margin-top', menuHeight)
+})
--- js/script.js
+++ js/script.js
@@ -1,16 +1,20 @@
-$('nav a, #vejaMais').click(function(e){
-    e.preventDefault();
-
-    let id = $(this).attr('href')
-
-    let altura = $(id).offset().top;
-
-    let menuHeight = $('nav').innerHeight();
-
-    $('html').animate({
-        scrollTop: altura - menuHeight
-    },1000);
-
+$(document).ready(()=> {
+    $('nav a, #vejaMais').click(function(e){
+        e.preventDefault();
+    
+        let id = $(this).attr('href')
+    
+        let altura = $(id).offset().top;
+    
+        let menuHeight = $('nav').innerHeight();
+    
+        $('html').animate({
+            scrollTop: altura - menuHeight
+        },1000);
+    
+    })
 })
+
+
 
 
